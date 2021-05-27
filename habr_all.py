808f59f62df63da17fa5ae05522c6c9bf783ef6a
User Name <99ww77@gmail.com>
2021-05-27 21:40:45 +0300

    Second commit

--- habr_all.py
+++ habr_all.py
@@ -6,7 +6,7 @@ def articles_and_links():
     resp =req.get(r'https://habr.com/ru/all/')
     soup = bs(resp.text, 'html.parser')
     articles_titles = soup.findAll('a', class_='post__title_link')
-    titless_links = {}
+    titles_links = {}
     for i in articles_titles:
-        titless_links[i.text] = i.get('href')
-    return titless_links
+        titles_links[i.text] = i.get('href')
+    return titles_links
